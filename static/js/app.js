e8f0ed1255b94be73e6bed8b11ebe948a496db11
Krzysztof Mioduszewski <kmioduszewski@gmail.com>
2015-06-13 19:39:03 +0200

    adding section views and icons, wip

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,39 @@
+var main = $('#main');
+var addBtn = $('#main-button-add');
+var backBtn = $('#main-button-back');
+
+var removeAction = function() {
+  main.removeClass('action');
+  main.removeClass('action-add');
+  main.removeClass('action-details');
+};
+
+var detailsClickHandler = function(event) {
+  removeAction();
+  main.toggleClass('action action-details');
+};
+
+addBtn.click(function() {
+  removeAction();
+  main.toggleClass('action action-add');
+}); 
+
+backBtn.click(function() {
+  removeAction();
+});
+
+$('#main-add-type-select').click(function(event) {
+  console.log(event);
+});
+
 function init_map(){
     var myOptions = {zoom:15, center:new google.maps.LatLng(52.221973,21.01772600000004), mapTypeId:google.maps.MapTypeId.ROADMAP, scrollwheel:false};
 
     var map = new google.maps.Map(document.getElementById("main-map"), myOptions);
     var marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(52.221973, 21.01772600000004)});
-    var infowindow = new google.maps.InfoWindow({content:"<b>Warsztat Warszawski</b><br/>Plac Konstytucji 4<br/>00-552 Warszawa" });
-    google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});
-    infowindow.open(map,marker);
-  }
+    google.maps.event.addListener(marker, "click", function(){
+      detailsClickHandler();
+    });
+}
 
 google.maps.event.addDomListener(window, 'load', init_map);
