5c8df4bea30f23118a8aa99865c7082e5efefc9f
Krzysztof Mioduszewski <kmioduszewski@gmail.com>
2015-06-27 19:44:51 +0200

    Wrapped map in iffe + exposed init, drawMarker, createMarker functions + main controller uses API to retrieve/update data and Map funtions to draw results on screen + indentation fixes

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,21 @@
 'use strict';
 
-/* global UI, API */
+/* global UI, API, Map */
 
 window.addEventListener('DOMContentLoaded', function() {
   if(localStorage.getItem('debug') === 'enabled') {
     UI.enableDebug();
   }
+
+  Map.init();
+  API.getMarkers(null, function(markers) {
+    Map.drawMarkers(markers);
+  });
+
+  UI.setAddMarkerHandler(function(markerOptions) {
+    var marker = Map.createMarker(markerOptions);
+    API.addMarker(marker.customInfo, function(savedMarker) {
+      marker.customInfo.id = savedMarker.id;
+    });
+  });
 });
--- static/js/map.js
+++ static/js/map.js
@@ -5,22 +5,24 @@
 /**
  * Created by ppaw on 2015-06-13.
  */
-var geocoder = new google.maps.Geocoder();
 
-var startLat = 52.4023366862351;
-var startLng = 16.925666755676275;
+(function(exports) {
+  var geocoder = new google.maps.Geocoder();
 
-var initialLocation = null;
+  var startLat = 52.4023366862351;
+  var startLng = 16.925666755676275;
 
-var map;
-var markers = [];
-var browserSupportFlag = false;
+  var initialLocation = null;
 
-var follow = true;
+  var map;
+  var markers = [];
+  var browserSupportFlag = false;
 
-var markerIconPrefix = 'static/img/markers/';
+  var follow = true;
 
-var markerIconMap = {
+  var markerIconPrefix = 'static/img/markers/';
+
+  var markerIconMap = {
     "low-curb":"przyjaznykraweznik.png",
     "high-curb":"wysokikraweznik.png",
     "stairs":"schody.png",
@@ -30,157 +32,164 @@ var markerIconMap = {
     "foot-bridge":"przejscienadziemne.png",
     "ramp":"pochylnia.png",
     "slope":"pochylosc.png"
-};
+  };
 
-var currentMarker;
+  var currentMarker;
 
-// Sets the map on all markers in the array.
-function setAllMap(map) {
+  // Sets the map on all markers in the array.
+  function setAllMap(map) {
     for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(map);
+      markers[i].setMap(map);
     }
-}
+  }
 
-// Removes the markers from the map, but keeps them in the array.
-function clearMarkers() {
+  // Removes the markers from the map, but keeps them in the array.
+  function clearMarkers() {
     setAllMap(null);
-}
+  }
 
-// Shows any markers currently in the array.
-function showMarkers() {
+  // Shows any markers currently in the array.
+  function showMarkers() {
     setAllMap(map);
-}
+  }
 
-function addMarkerToMap(markerOptions) {
+  function addMarkerToMap(markerOptions, draggable) {
     var latLng = new google.maps.LatLng(markerOptions.lat, markerOptions.lng);
 
     var iconPath = null;
     var image = null;
     if (markerIconMap[markerOptions.kind]) {
-        iconPath = markerIconPrefix + markerIconMap[markerOptions.kind]
-        image = {
-            url: iconPath
-        };
+      iconPath = markerIconPrefix + markerIconMap[markerOptions.kind]
+      image = {
+          url: iconPath
+      };
     }
 
     var marker = new google.maps.Marker({
-        position: latLng,
-        title: markerOptions.kind,
-        map: map,
-        icon: image,
-        draggable: true,
-        customInfo: markerOptions
+      position: latLng,
+      title: markerOptions.kind,
+      map: map,
+      icon: image,
+      draggable: true,
+      customInfo: markerOptions
     });
 
     markers.push(marker);
 
     google.maps.event.addListener(marker, 'click', function(e) {
-        currentMarker = marker;
+      currentMarker = marker;
 
-        UI.showDetails(marker.customInfo);
+      UI.showDetails(marker.customInfo);
     });
 
     google.maps.event.addListener(marker, 'dragend', function() {
-        updateMarker(marker);
+      updateMarker(marker);
     });
-}
 
-function getMarkers(bounds) {
+    return marker;
+  }
+
+  function getMarkers(bounds) {
     API.getMarkers(bounds, addMarkersToMap.bind(this));
-}
+  }
 
-function addMarkersToMap(markersOptions) {
+  function addMarkersToMap(markersOptions) {
     for (var i in markersOptions) {
-        addMarkerToMap(markersOptions[i]);
+      addMarkerToMap(markersOptions[i]);
     }
-}
+  }
 
-function newMarker(markerOptions) {
+  function newMarker(markerOptions) {
     var defaultMarkerOptions = {
-        lat: initialLocation.lat(),
-        lng: initialLocation.lng(),
-        state: 'ok'
+      lat: initialLocation.lat(),
+      lng: initialLocation.lng(),
+      state: 'ok'
     };
 
-    markerOptions = $.extend(defaultMarkerOptions, markerOptions);
-    API.addMarker(markerOptions, refresh.bind(this));
-}
+    var marker = $.extend(defaultMarkerOptions, markerOptions);
+    return addMarkerToMap(marker);
+  }
 
-function updateMarker(marker) {
+  function updateMarker(marker) {
     var m = marker.customInfo;
 
     m.lat = marker.getPosition().lat();
     m.lng = marker.getPosition().lng();
     API.updateMarker(m, refresh.bind(this));
-}
+  }
 
-function deleteMarker(marker) {
+  function deleteMarker(marker) {
     var m = marker.customInfo || marker;
     API.deleteMarker(m, refresh.bind(this));
-}
+  }
 
-function refresh() {
+  function refresh(markers) {
     clearMarkers();
-    var m = getMarkers();
+    var m = markers || getMarkers();
     addMarkersToMap(m);
-}
+  }
 
-$(function() {
+  function init() {
     initialLocation = new google.maps.LatLng(startLat, startLng);
     map = new google.maps.Map(document.getElementById('main-map'), {
-        zoom: 19,
-        center: initialLocation,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        disableDefaultUI: true,
-        zoomControl: true
+      zoom: 19,
+      center: initialLocation,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true,
+      zoomControl: true
     });
 
     var GeoMarker = new GeolocationMarker(map);
 
     GeoMarker.setCircleOptions({
-        visible: false
+      visible: false
     });
 
     GeoMarker.setMarkerOptions({
-        zIndex: 999
+      zIndex: 999
     });
 
     // Try W3C Geolocation (Preferred)
     if(navigator.geolocation) {
-        browserSupportFlag = true;
-        navigator.geolocation.getCurrentPosition(function(position) {
-            initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-            map.setCenter(initialLocation);
-        }, function() {
-            handleNoGeolocation(browserSupportFlag);
-        });
+      browserSupportFlag = true;
+      navigator.geolocation.getCurrentPosition(function(position) {
+        initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+        map.setCenter(initialLocation);
+      }, function() {
+        handleNoGeolocation(browserSupportFlag);
+      });
     }
     // Browser doesn't support Geolocation
     else {
-        browserSupportFlag = false;
-        handleNoGeolocation(browserSupportFlag);
+      browserSupportFlag = false;
+      handleNoGeolocation(browserSupportFlag);
     }
 
     function handleNoGeolocation(errorFlag) {
-        if (errorFlag === true) {
-            alert('Geolocation service failed.');
-        } else {
-            alert('Your browser doesn\'t support geolocation. We\'ve placed you in Siberia.');
-        }
+      if (errorFlag === true) {
+        alert('Geolocation service failed.');
+      } else {
+        alert('Your browser doesn\'t support geolocation. We\'ve placed you in Siberia.');
+      }
     }
 
-    refresh();
-
-    UI.setAddMarkerHandler(newMarker);
     UI.setDeleteMarkerHandler(deleteMarker);
     UI.setUpvoteMarkerHandler(function(marker) {
-        marker.votes = (marker.votes) ? marker.votes + 1 : 1;
-        API.updateMarker(marker);
-        UI.showDetails(marker);
+      marker.votes = (marker.votes) ? marker.votes + 1 : 1;
+      API.updateMarker(marker);
+      UI.showDetails(marker);
     });
     UI.setFailureMarkerHandler(function(marker) {
-        marker.state = 'failure';
-        API.updateMarker(marker);
-        UI.showDetails(marker);
+      marker.state = 'failure';
+      API.updateMarker(marker);
+      UI.showDetails(marker);
     });
-});
+  }
+
+  exports.Map = {
+    init: init,
+    drawMarkers: refresh,
+    createMarker: newMarker,
+  };
+
+})((typeof exports === 'undefined') ? window : exports);
