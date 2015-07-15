e96476361808c28c7014592d76b94afd35158b57
Krzysztof Mioduszewski <kmioduszewski@gmail.com>
2015-07-15 12:03:22 +0200

    set up nightwatch.js and first e2e test

---
+++ tests/e2e/main-screen.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'Main screen visbility': function(browser) {
+    browser.url('http://localhost:8080');
+
+    browser.expect.element('body').to.be.present.before(1000);
+    // header
+    browser.expect.element('#main nav.navbar').to.be.visible;
+    browser.expect.element('#main nav #main-button-back').to.be.visible;
+    browser.expect.element('#main nav div.main-title').to.be.visible;
+    browser.expect.element('#main nav #main-button-add').to.be.visible;
+    
+    // map
+    browser.expect.element('#main #main-map div.gm-style').to.be.visible;
+
+    browser.end();
+  }
+};
